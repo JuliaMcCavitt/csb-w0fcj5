b70caaef5ce7f81092dc68c9711c815596ec1b2b
Code Sandbox <hello@codesandbox.io>
2023-01-31 22:31:50 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.31bb4aac6f7a41f6924bffb2d2359767.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00c5fbd05c91484eafb3a897a3b1e725",
+    "url": "https://juliamccavitt.github.io/csb-w0fcj5/index.html"
+  },
+  {
+    "revision": "cbf781e6d59a21faff19",
+    "url": "https://juliamccavitt.github.io/csb-w0fcj5/static/css/main.2412c53a.chunk.css"
+  },
+  {
+    "revision": "b0b30389b7929460ec61",
+    "url": "https://juliamccavitt.github.io/csb-w0fcj5/static/js/2.4a4f1b68.chunk.js"
+  },
+  {
+    "revision": "56bced16cf8e36205306aa52a1493ddb",
+    "url": "https://juliamccavitt.github.io/csb-w0fcj5/static/js/2.4a4f1b68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbf781e6d59a21faff19",
+    "url": "https://juliamccavitt.github.io/csb-w0fcj5/static/js/main.865730f5.chunk.js"
+  },
+  {
+    "revision": "bc651512376eaf2d265a",
+    "url": "https://juliamccavitt.github.io/csb-w0fcj5/static/js/runtime-main.183feb18.js"
+  }
+]);
